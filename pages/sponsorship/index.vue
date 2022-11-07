a391bab5aad87db9a5878aff8f72e021a0130151
KnappLuke9 <lukeandhyebin@gmail.com>
2022-11-07 10:13:55 +0800

    WIP iframe

--- pages/sponsorship/index.vue
+++ pages/sponsorship/index.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app>
-    <main class="v-flex justify-content-center">
+    <!-- <main class="v-flex justify-content-center">
       <v-container class="px-md-16 mx-lg-auto">
         <v-container style="max-width: 900px">
           <div type="text" justify="center" readonly v-html="foundLanding.attributes.content"></div>
         </v-container>
       </v-container>
-    </main>
+    </main> -->
+    <iframe loading="lazy" src="https://embed.revolutionise.com.au/floorballwa/pointscores/12434/&amp;d=21265" width="100%" height="1000"></iframe>
   </v-app>
 </template>
 
